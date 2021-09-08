083be07e8c4bb6f247760d358d00a0febcc54140
Niall <niall.p.deehan@gmail.com>
2021-09-08 16:47:22 +0200

    Added Optimized Model

--- src/main/java/com/example/workflow/RunNextLapDelegate.java
+++ src/main/java/com/example/workflow/RunNextLapDelegate.java
@@ -2,6 +2,7 @@ package com.example.workflow;
 
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.camunda.bpm.model.xml.instance.ModelElementInstance;
 import org.camunda.feel.syntaxtree.In;
 
 import javax.inject.Named;
@@ -12,6 +13,13 @@ public class RunNextLapDelegate implements JavaDelegate {
     @Override
     public void execute(DelegateExecution delegateExecution) throws Exception {
 
+        String messageRef = delegateExecution.getBpmnModelElementInstance().getAttributeValue("messageRef");
+        System.out.println("Message Ref: "+  messageRef);
+        String messageName =
+                delegateExecution.getBpmnModelInstance().getModelElementById(messageRef).getAttributeValue(
+                "name");
+
+
         Map<String, Object> vars = delegateExecution.getVariables();
         Integer totalLaps = (Integer) vars.get("totalLaps");
         Integer lapsCompleted = (Integer) vars.get("lapsCompleted");
@@ -20,7 +28,7 @@ public class RunNextLapDelegate implements JavaDelegate {
         System.out.println("Current Laps Completed: " +lapsCompleted);
         System.out.println("Total Race Laps: " +totalLaps);
 
-        delegateExecution.getProcessEngineServices().getRuntimeService().createMessageCorrelation("NextLap")
+        delegateExecution.getProcessEngineServices().getRuntimeService().createMessageCorrelation(messageName)
                 .setVariables(vars)
                 .processInstanceBusinessKey(delegateExecution.getBusinessKey())
                 .correlate();
